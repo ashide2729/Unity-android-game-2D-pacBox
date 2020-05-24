b1129bacd5ef834ecbd0efebc12f75e8955abade
Ashish Deshwal <deshwalashish.deshwal5@gmail.com>
2020-05-24 08:07:24 +0530

    Assets added

---
+++ Assets/Square.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Square : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float moveSpeed;
+    public float rotateAmount;
+    float rot;
+    int score = 0;
+    public GameObject WinText;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (mousePos.x < 0)
+            {
+                rot = rotateAmount;
+            }
+            else
+            {
+                rot = -rotateAmount;
+            }
+
+            transform.Rotate(0, 0, rot);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Boundary")
+        {
+            transform.Rotate(0, 0, 180);
+        }
+        if (collision.gameObject.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+            score++;
+
+            if (score >= 6)
+            {
+                print("Level Completed");
+                WinText.SetActive(true);
+            }
+        }
+        else if(collision.gameObject.tag == "Danger")
+        {
+            SceneManager.LoadScene("Scene1");
+
+        }
+    }
+}
